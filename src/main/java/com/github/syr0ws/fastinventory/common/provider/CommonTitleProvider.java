bbd55f5fa33836282519f076c0c600939868391b
Syrows <clement.gho@gmail.com>
2024-09-09 23:48:45 +0200

    removing unused import

--- src/main/java/com/github/syr0ws/fastinventory/common/provider/CommonTitleProvider.java
+++ src/main/java/com/github/syr0ws/fastinventory/common/provider/CommonTitleProvider.java
@@ -1,7 +1,6 @@
 package com.github.syr0ws.fastinventory.common.provider;
 
 import com.github.syr0ws.fastinventory.api.config.InventoryConfig;
-import com.github.syr0ws.fastinventory.api.i18n.I18n;
 import com.github.syr0ws.fastinventory.api.placeholder.PlaceholderManager;
 import com.github.syr0ws.fastinventory.api.provider.InventoryProvider;
 import com.github.syr0ws.fastinventory.api.provider.Provider;
