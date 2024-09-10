c4d90028ab3a08699506b1ed021561509138350d
Syrows <clement.gho@gmail.com>
2024-09-10 18:46:11 +0200

    adding pagination placeholders

---
+++ src/main/java/com/github/syr0ws/fastinventory/common/placeholder/pagination/CurrentPagePlaceholder.java
@@ -0,0 +1,35 @@
+package com.github.syr0ws.fastinventory.common.placeholder.pagination;
+
+import com.github.syr0ws.fastinventory.api.FastInventory;
+import com.github.syr0ws.fastinventory.api.pagination.Pagination;
+import com.github.syr0ws.fastinventory.api.pagination.PaginationModel;
+import com.github.syr0ws.fastinventory.api.placeholder.Placeholder;
+import com.github.syr0ws.fastinventory.api.util.Context;
+import com.github.syr0ws.fastinventory.common.CommonContextKey;
+
+public class CurrentPagePlaceholder implements Placeholder {
+
+    @Override
+    public String getName() {
+        return "%current_page%";
+    }
+
+    @Override
+    public String getValue(Context context) {
+
+        FastInventory inventory = context.getData(CommonContextKey.INVENTORY.name(), FastInventory.class);
+        String paginationId = context.getData(CommonContextKey.PAGINATION_ID.name(), String.class);
+
+        Pagination<?> pagination = inventory.getPagination(paginationId)
+                .orElseThrow(() -> new IllegalStateException(String.format("No pagination with id %s found", paginationId)));
+
+        PaginationModel<?> model = pagination.getModel();
+
+        return Integer.toString(model.getCurrentPage());
+    }
+
+    @Override
+    public boolean accept(Context context) {
+        return context.hasData(CommonContextKey.PAGINATION_ID.name());
+    }
+}
