5cd053cd1c7a22218fbdad9847c158749a96e47d
Syrows <clement.gho@gmail.com>
2024-10-20 19:06:01 +0200

    factorizing code

---
+++ src/main/java/com/github/syr0ws/fastinventory/internal/config/yaml/action/YamlCommandActionLoader.java
@@ -0,0 +1,23 @@
+package com.github.syr0ws.fastinventory.internal.config.yaml.action;
+
+import com.github.syr0ws.fastinventory.api.config.exception.InventoryConfigException;
+import com.github.syr0ws.fastinventory.common.config.yaml.YamlCommonActionLoader;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.List;
+
+public abstract class YamlCommandActionLoader extends YamlCommonActionLoader {
+
+    private static final String COMMANDS_KEY = "commands";
+
+    protected List<String> loadCommands(ConfigurationSection section) throws InventoryConfigException {
+
+        if (!section.isList(COMMANDS_KEY)) {
+            throw new InventoryConfigException(String.format("Property '%s.%s' not found or is not a list", COMMANDS_KEY, section.getCurrentPath()));
+        }
+
+        return section.getStringList(COMMANDS_KEY).stream()
+                .map(command -> command.startsWith("/") ? command.substring(1) : command)
+                .toList();
+    }
+}
--- src/main/java/com/github/syr0ws/fastinventory/internal/config/yaml/action/YamlConsoleCommandActionLoader.java
+++ src/main/java/com/github/syr0ws/fastinventory/internal/config/yaml/action/YamlConsoleCommandActionLoader.java
@@ -4,28 +4,18 @@ import com.github.syr0ws.fastinventory.api.action.ClickAction;
 import com.github.syr0ws.fastinventory.api.action.ClickType;
 import com.github.syr0ws.fastinventory.api.config.exception.InventoryConfigException;
 import com.github.syr0ws.fastinventory.common.action.ConsoleCommandAction;
-import com.github.syr0ws.fastinventory.common.config.yaml.YamlCommonActionLoader;
 import org.bukkit.configuration.ConfigurationSection;
 
 import java.util.List;
 import java.util.Set;
 
-public class YamlConsoleCommandActionLoader extends YamlCommonActionLoader {
-
-    private static final String COMMANDS_KEY = "commands";
+public class YamlConsoleCommandActionLoader extends YamlCommandActionLoader {
 
     @Override
     public ClickAction load(ConfigurationSection section) throws InventoryConfigException {
 
         Set<ClickType> clickTypes = super.loadClickTypes(section);
-
-        if (!section.isList(COMMANDS_KEY)) {
-            throw new InventoryConfigException(String.format("Property '%s.%s' not found or is not a list", COMMANDS_KEY, section.getCurrentPath()));
-        }
-
-        List<String> commands = section.getStringList(COMMANDS_KEY).stream()
-                .map(command -> command.startsWith("/") ? command.substring(1) : command)
-                .toList();
+        List<String> commands = super.loadCommands(section);
 
         return new ConsoleCommandAction(clickTypes, commands);
     }
