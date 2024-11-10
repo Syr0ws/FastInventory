9098f011e22ee634fd36092b86306249d0e9deaa
Syrows <clement.gho@gmail.com>
2024-11-10 17:39:43 +0100

    adding new action

---
+++ src/main/java/com/github/syr0ws/fastinventory/common/inventory/action/UpdateContentAction.java
@@ -0,0 +1,27 @@
+package com.github.syr0ws.fastinventory.common.inventory.action;
+
+import com.github.syr0ws.fastinventory.api.inventory.FastInventory;
+import com.github.syr0ws.fastinventory.api.inventory.action.ClickType;
+import com.github.syr0ws.fastinventory.api.inventory.event.FastInventoryClickEvent;
+
+import java.util.Set;
+
+public class UpdateContentAction extends CommonAction {
+
+    public static final String ACTION_NAME = "UPDATE_CONTENT";
+
+    public UpdateContentAction(Set<ClickType> clickTypes) {
+        super(clickTypes);
+    }
+
+    @Override
+    public void execute(FastInventoryClickEvent event) {
+        FastInventory inventory = event.getFastInventory();
+        inventory.updateContent();
+    }
+
+    @Override
+    public String getName() {
+        return ACTION_NAME;
+    }
+}
