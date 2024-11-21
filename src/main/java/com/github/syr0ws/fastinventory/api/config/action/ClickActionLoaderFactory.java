aefbef4e83e53e881a379823b89c03770af1795b
Syrows <clement.gho@gmail.com>
2024-11-21 18:41:29 +0100

    adding javadoc

--- src/main/java/com/github/syr0ws/fastinventory/api/config/action/ClickActionLoaderFactory.java
+++ src/main/java/com/github/syr0ws/fastinventory/api/config/action/ClickActionLoaderFactory.java
@@ -2,11 +2,34 @@ package com.github.syr0ws.fastinventory.api.config.action;
 
 import java.util.Set;
 
+/**
+ * Factory interface for managing and providing {@link ClickActionLoader} instances.
+ *
+ * @param <T> The type of the data source handled by the {@link ClickActionLoader}.
+ */
 public interface ClickActionLoaderFactory<T> {
 
+    /**
+     * Registers a new {@link ClickActionLoader}.
+     *
+     * @param loader The {@link ClickActionLoader} to register. Must not be {@code null}.
+     */
     void addLoader(ClickActionLoader<T> loader);
 
+    /**
+     * Retrieves a {@link ClickActionLoader} by its name.
+     *
+     * @param name The name of the loader to retrieve. Must not be {@code null}.
+     * @return The corresponding {@link ClickActionLoader}, or {@code null} if no loader
+     *         with the specified name is registered.
+     */
     ClickActionLoader<T> getLoader(String name);
 
+    /**
+     * Retrieves all registered {@link ClickActionLoader} instances.
+     *
+     * @return A set of all registered {@link ClickActionLoader} instances. If no loaders
+     *         are registered, the set will be empty.
+     */
     Set<ClickActionLoader<T>> getLoaders();
 }
--- src/main/java/com/github/syr0ws/fastinventory/api/config/dao/InventoryConfigDAO.java
+++ src/main/java/com/github/syr0ws/fastinventory/api/config/dao/InventoryConfigDAO.java
@@ -6,9 +6,37 @@ import com.github.syr0ws.fastinventory.api.config.exception.InventoryConfigExcep
 import java.nio.file.Path;
 import java.util.Set;
 
+/**
+ * Data Access Object (DAO) interface for loading inventory configurations.
+ */
 public interface InventoryConfigDAO {
 
+    /**
+     * Loads an {@link InventoryConfig} from a specified file.
+     *
+     * @param file The path to the configuration file. Must not be {@code null}.
+     * @return The loaded {@link InventoryConfig}.
+     * @throws IllegalArgumentException If file is {@code null}
+     * @throws InventoryConfigException If an error occurs while loading or parsing the configuration file.
+     */
     InventoryConfig loadConfig(Path file) throws InventoryConfigException;
 
+    /**
+     * Loads all {@link InventoryConfig} instances from a specified folder.
+     * <p>
+     * This method scans the folder for valid configuration files, attempts to load them,
+     * and returns a set of all successfully parsed {@link InventoryConfig} instances.
+     * <br>
+     * If an error occurs while loading a specific configuration file, that file will be skipped,
+     * and a stack trace will be generated.
+     * </p>
+     *
+     * @param folder The path to the folder containing configuration files. Must not be {@code null}.
+     * @return A set of successfully loaded {@link InventoryConfig} instances. If no valid configuration files
+     *         are found, the set will be empty.
+     * @throws IllegalArgumentException If the {@code folder} parameter is {@code null}.
+     * @throws InventoryConfigException If the given path does not point to a folder or if an error occurs
+     *                                   while accessing the folder.
+     */
     Set<InventoryConfig> loadConfigs(Path folder) throws InventoryConfigException;
 }
